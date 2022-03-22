b4a16c9c609c612f431c9be42b507d7dd012bcac
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2022-03-22 23:32:22 +0500

    input-elements

--- aap.js
+++ aap.js
@@ -2,6 +2,17 @@
 console.log("ameer ali ajmali")
 alert("hello world")
 confirm("hello confirm")
+var num1 = 2;
+var num4 = 4;
+var num8 = 8;
+var num7 = 7;
+var num12 = 12;
+var num18 = 18;
+var num89 = 89;
+document.write(num1+num4+num18*20)
+var name=prompt("Enter your name")
+document.write[name]("hello")
+
 
 
 
