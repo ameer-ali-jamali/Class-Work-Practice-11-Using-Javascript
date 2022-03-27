37deca505faa4960ac908b7b7594bdd5b00ac817
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2022-03-27 18:02:05 +0500

    Javascript-if else practice

--- aap.js
+++ aap.js
@@ -1,36 +1,33 @@
-//  document.write(num1+num4+num18*20)
-// console.log("ameer ali ajmali")
-// alert("hello world")
-// confirm("hello confirm")
-// var num1 = 2;
-// var num4 = 4;
-// var num8 = 8;
-// var num7 = 7;
-// var num12 = 12;
-// var num18 = 18;
-// var num89 = 89;
-// document.write(num1+num4+num18*20)
-// var Name=prompt("Enter your Name")
-// document.write(Name+"Hello")
-// var Name=prompt("Enter you Name")
-// var Age=prompt("Enter you Age")
-// Age =parseInt(Age)
-// document.write(`Hello My Name is`+ Name +`I am`+ Age+`Years old`)
-// document.write(num8*num7 "Multipli Result")
-// document.write(num12-num18 "Minus Result")
-// var  Num=prompt(`Enter Any Number From 1 To 10000000`)
-// var Num9=prompt(`Enter Any Number From 1 To 10000000`)
-// Num=parseInt(Num)
-// Num9=parseInt(Num9)
-// document.write(Num+Num9)
-// document.write( "  Subtriction Results")
-// document.write("<hr>")
-// document.write(Num-Num9)
-// document.write( "</br>  Minus Results")
-// document.write("<hr>")
-// document.write(Num*Num9)
-// document.write( "</br>  Multipli Results")
-// aler(a==0)
+ document.write(num1+num4+num18*20)
+console.log("ameer ali ajmali")
+alert("hello world")
+confirm("hello confirm")
+var num1 = 2;
+var num4 = 4;
+var num8 = 8;
+var num7 = 7;
+var num12 = 12;
+var num18 = 18;
+var num89 = 89;
+document.write(num1+num4+num18*20)
+var Name=prompt("Enter your Name")
+document.write(Name+"Hello")
+var Name=prompt("Enter you Name")
+var Age=prompt("Enter you Age")
+Age =parseInt(Age)
+var  Num=prompt(`Enter Any Number From 1 To 10000000`)
+var Num9=prompt(`Enter Any Number From 1 To 10000000`)
+Num=parseInt(Num)
+Num9=parseInt(Num9)
+document.write(Num+Num9)
+document.write( "  Subtriction Results")
+document.write("<hr>")
+document.write(Num-Num9)
+document.write( "</br>  Minus Results")
+document.write("<hr>")
+document.write(Num*Num9)
+document.write( "</br>  Multipli Results")
+aler(a==0)
 var Age=prompt("Enter you Age")
 Age =parseInt(Age)
 // document.write(Age)
